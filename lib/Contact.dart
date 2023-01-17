0613310ae7959609ad89a58cf5d3a77d23a3c7bb
ploydsr <p.dusarux@gmail.com>
2023-01-17 20:14:40 +0700

    can search list contact

--- lib/Contact.dart
+++ lib/Contact.dart
@@ -1,8 +1,45 @@
 class Contact {
-  String name;
-  String nickname;
-  String position;
-  String img;
+  final String name;
+  final String nickname;
+  final String position;
+  final String img;
 
-  Contact(this.name, this.nickname, this.position, this.img);
+  const Contact(
+      {required this.name,
+      required this.nickname,
+      required this.position,
+      required this.img});
 }
+
+const allContacts = [
+  Contact(
+      name: 'Adam Live',
+      nickname: 'Adam',
+      position: 'UX/UI Designer',
+      img: 'assets/images/img1.png'),
+  Contact(
+      name: 'BA Gala',
+      nickname: 'Gaga',
+      position: 'UX/UI Designer',
+      img: 'assets/images/img1.png'),
+  Contact(
+      name: 'Adam Rally',
+      nickname: 'Rallyr',
+      position: 'UX/UI Designer',
+      img: 'assets/images/img4.png'),
+  Contact(
+      name: 'Cally Lim',
+      nickname: 'Call',
+      position: 'UX/UI Designer',
+      img: 'assets/images/img3.png'),
+  Contact(
+      name: 'BB Jally',
+      nickname: 'Jall',
+      position: 'UX/UI Designer',
+      img: 'assets/images/img4.png'),
+  Contact(
+      name: 'Cany Kit',
+      nickname: 'Candy',
+      position: 'UX/UI Designer',
+      img: 'assets/images/img2.png')
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:basic_flutter/ContactPage.dart';
 import 'package:flutter/material.dart';
 import './Contact.dart';
+import 'package:azlistview/azlistview.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: "My App",
-        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        theme: ThemeData(primarySwatch: Colors.blue),
         home: MyHomePage());
   }
 }
@@ -29,35 +30,63 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   // group data
-  List<Contact> contact = [
-    Contact("Adam Live", "Adam", "UX/UI Designer", "assets/images/img1.png"),
-    Contact("Adam Rally", "Rally", "UX/UI Designer", "assets/images/img4.png"),
-    Contact("BA Gala", "Gaga", "UX/UI Designer", "assets/images/img1.png"),
-    Contact("BB Jally", "Jall", "UX/UI Designer", "assets/images/img4.png"),
-    Contact("Cany Kit", "Candy", "UX/UI Designer", "assets/images/img2.png"),
-    Contact("Cally Lim", "Call", "UX/UI Designer", "assets/images/img3.png")
-  ];
+  List<Contact> contacts = allContacts;
 
   @override
   Widget build(BuildContext context) {
+    List<Contact> sortedContacts = List.from(contacts)
+      ..sort((a, b) => a.name.toLowerCase().compareTo(b.name.toLowerCase()));
     return Scaffold(
-        appBar: AppBar(title: Text("My App", style: TextStyle(fontSize: 20))),
-        body: ListView.builder(
-          itemBuilder: (BuildContext context, int index) {
-            Contact mem = contact[index];
-            return ListTile(
-              leading: Image.asset(mem.img),
-              title: Text("${mem.name} (${mem.nickname})",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-              subtitle: Text(mem.position, style: TextStyle(fontSize: 10)),
-              onTap: () {
-                print("Contact:>" + mem.name);
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ContactPage(mem)));
-              },
-            );
-          },
-          itemCount: contact.length,
+        appBar: AppBar(title: Text("Contact", style: TextStyle(fontSize: 20))),
+        body: Column(
+          children: [
+            Container(
+              decoration:
+                  BoxDecoration(color: Color.fromARGB(255, 246, 246, 246)),
+              child: Padding(
+                padding:
+                    EdgeInsets.only(top: 40, right: 20, bottom: 20, left: 20),
+                child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(7.0)),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        labelText: 'Search',
+                        prefixIcon: Icon(Icons.search),
+                      ),
+                      onChanged: searchContact,
+                    )),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: contacts.length,
+                itemBuilder: (context, index) {
+                  final contact = sortedContacts[index];
+
+                  return ListTile(
+                    leading: Image.asset(contact.img),
+                    title: Text("${contact.name} (${contact.nickname})"),
+                    subtitle: Text(contact.position),
+                  );
+                },
+              ),
+            ),
+          ],
         ));
+  }
+
+  void searchContact(String query) {
+    final suggestions = allContacts.where((contact) {
+      final contactName = contact.name.toLowerCase();
+      final input = query.toLowerCase();
+
+      return contactName.contains(input);
+    }).toList();
+    setState(() {
+      contacts = suggestions;
+    });
   }
 }
